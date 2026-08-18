e04e425c05bd018a82bc411355a147a22e08c6e9
nkokic <nikola1.kokic@gmail.com>
2026-08-18 13:44:11 +0200

    clean up

--- src/lv3/robot_control/setup.py
+++ src/lv3/robot_control/setup.py
@@ -13,8 +13,8 @@ setup(
     ],
     install_requires=['setuptools'],
     zip_safe=True,
-    maintainer='stjepan',
-    maintainer_email='stjepan@todo.todo',
+    maintainer='nikola',
+    maintainer_email='nikola@todo.todo',
     description='TODO: Package description',
     license='TODO: License declaration',
     extras_require={
